3b4cf0ef52b8ab96c2478c7fe0a96f3ccc02c01e
Федорова Антонина <32134036+tonyafedorova@users.noreply.github.com>
2017-11-01 18:02:08 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace lab4_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1ReadTime = new System.Windows.Forms.TextBox();
+            this.textBox1FileReadCount = new System.Windows.Forms.TextBox();
+            this.buttonExact = new System.Windows.Forms.Button();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.buttonApprox = new System.Windows.Forms.Button();
+            this.textBoxMaxDist = new System.Windows.Forms.TextBox();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.textBoxApproxTime = new System.Windows.Forms.TextBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonSaveReport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxExactTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(24, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Чтение из файла";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1ReadTime
+            // 
+            this.textBox1ReadTime.Location = new System.Drawing.Point(529, 29);
+            this.textBox1ReadTime.Name = "textBox1ReadTime";
+            this.textBox1ReadTime.Size = new System.Drawing.Size(135, 26);
+            this.textBox1ReadTime.TabIndex = 1;
+            // 
+            // textBox1FileReadCount
+            // 
+            this.textBox1FileReadCount.Location = new System.Drawing.Point(529, 71);
+            this.textBox1FileReadCount.Name = "textBox1FileReadCount";
+            this.textBox1FileReadCount.Size = new System.Drawing.Size(135, 26);
+            this.textBox1FileReadCount.TabIndex = 2;
+            // 
+            // buttonExact
+            // 
+            this.buttonExact.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.buttonExact.Location = new System.Drawing.Point(24, 181);
+            this.buttonExact.Name = "buttonExact";
+            this.buttonExact.Size = new System.Drawing.Size(137, 58);
+            this.buttonExact.TabIndex = 3;
+            this.buttonExact.Text = "Четкий поиск";
+            this.buttonExact.UseVisualStyleBackColor = false;
+            this.buttonExact.Click += new System.EventHandler(this.buttonExact_Click);
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(250, 141);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(193, 26);
+            this.textBoxFind.TabIndex = 4;
+            // 
+            // buttonApprox
+            // 
+            this.buttonApprox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.buttonApprox.Location = new System.Drawing.Point(24, 260);
+            this.buttonApprox.Name = "buttonApprox";
+            this.buttonApprox.Size = new System.Drawing.Size(137, 74);
+            this.buttonApprox.TabIndex = 5;
+            this.buttonApprox.Text = "Параллельный нечеткий поиск:";
+            this.buttonApprox.UseVisualStyleBackColor = false;
+            this.buttonApprox.Click += new System.EventHandler(this.buttonApprox_Click);
+            // 
+            // textBoxMaxDist
+            // 
+            this.textBoxMaxDist.Location = new System.Drawing.Point(601, 216);
+            this.textBoxMaxDist.Name = "textBoxMaxDist";
+            this.textBoxMaxDist.Size = new System.Drawing.Size(135, 26);
+            this.textBoxMaxDist.TabIndex = 6;
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 20;
+            this.listBoxResult.Location = new System.Drawing.Point(24, 353);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(712, 184);
+            this.listBoxResult.TabIndex = 8;
+            // 
+            // textBoxApproxTime
+            // 
+            this.textBoxApproxTime.Location = new System.Drawing.Point(601, 260);
+            this.textBoxApproxTime.Name = "textBoxApproxTime";
+            this.textBoxApproxTime.Size = new System.Drawing.Size(135, 26);
+            this.textBoxApproxTime.TabIndex = 9;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.Red;
+            this.buttonExit.Location = new System.Drawing.Point(582, 563);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(154, 35);
+            this.buttonExit.TabIndex = 10;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonSaveReport
+            // 
+            this.buttonSaveReport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonSaveReport.Location = new System.Drawing.Point(24, 563);
+            this.buttonSaveReport.Name = "buttonSaveReport";
+            this.buttonSaveReport.Size = new System.Drawing.Size(154, 32);
+            this.buttonSaveReport.TabIndex = 11;
+            this.buttonSaveReport.Text = "Отчет";
+            this.buttonSaveReport.UseVisualStyleBackColor = false;
+            this.buttonSaveReport.Click += new System.EventHandler(this.buttonSaveReport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Время чтения из файла:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Количество уникальных слов в тексте:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Слово для поиска:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(396, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(182, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Время четкого поиска:";
+            // 
+            // textBoxExactTime
+            // 
+            this.textBoxExactTime.Location = new System.Drawing.Point(601, 175);
+            this.textBoxExactTime.Name = "textBoxExactTime";
+            this.textBoxExactTime.Size = new System.Drawing.Size(135, 26);
+            this.textBoxExactTime.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(188, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(390, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Максимальное расстояние для нечеткого поиска:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(365, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(200, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Время нечеткого поиска:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(780, 607);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxExactTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSaveReport);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.textBoxApproxTime);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.textBoxMaxDist);
+            this.Controls.Add(this.buttonApprox);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.buttonExact);
+            this.Controls.Add(this.textBox1FileReadCount);
+            this.Controls.Add(this.textBox1ReadTime);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1ReadTime;
+        private System.Windows.Forms.TextBox textBox1FileReadCount;
+        private System.Windows.Forms.Button buttonExact;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.Button buttonApprox;
+        private System.Windows.Forms.TextBox textBoxMaxDist;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.TextBox textBoxApproxTime;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonSaveReport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxExactTime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
